472b74b81418282c8fd78c7d54332e1740b35769
lok <cleiton_biou@hotmail.com>
2020-03-24 22:49:01 -0300

    back-end
    Signed-off-by: lok <cleiton_biou@hotmail.com>

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const port = process.env.PORT || 3333
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, () => { console.log(`Server running at 'http://localhost:${port}/'`) })
